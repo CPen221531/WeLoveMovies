5030ebf7c2e4890e85360d754f79dd7adf2fb12b
C. Pen <c.pen@Cs-iMac.risebroadband.com>
2024-12-15 17:01:39 -0600

    Initial commit for gh-pages deployment

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Welcome to We Love Movies!</h2>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>We Love Movies</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
