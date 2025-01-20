3e7f409858a2ce7e67a412245327e2f469c03dbe
C. Pen <c.pen@Cs-iMac.risebroadband.com>
2025-01-20 00:17:01 -0600

    Update WeLoveMovies project files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import './styles.css';  // Import the styles.css file
+import React from "react";
+import Navbar from "./components/Navbar";
+import Movies from "./Movies"; // Import the Movies component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <main>
+        <h2 className='ttpg'>Welcome to We Love Movies</h2>
+        <p className='ttpg'>Your ultimate movie database and reviews platform.</p>
+        <Movies /> {/* Render the Movies component */}
+      </main>
     </div>
   );
 }
